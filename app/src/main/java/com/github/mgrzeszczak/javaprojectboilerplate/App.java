fbddc8650308fad6b8b74a81cde3a44e992494c3
Maciej Grzeszczak <maciejgrzeszczak@gmail.com>
2017-09-24 15:14:38 +0200

    Add lombok

--- app/src/main/java/com/github/mgrzeszczak/javaprojectboilerplate/App.java
+++ app/src/main/java/com/github/mgrzeszczak/javaprojectboilerplate/App.java
@@ -1,14 +1,12 @@
 package com.github.mgrzeszczak.javaprojectboilerplate;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class App {
 
-    private final static Logger LOGGER = LogManager.getLogger(App.class);
-
     public static void main(String[] args) {
-        LOGGER.info("Hello world");
+        log.info("Hello world");
     }
 
 }
